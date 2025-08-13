689d78302b32efe9e9b69f7165ffbbd793fdb26d
JaguarGT <josuerivdl@gmail.com>
2025-08-12 20:54:26 -0600

    Side bar Productos - Front
    Creación del side bar para el front de productos

--- frontend/src/app/producto/page.tsx
+++ frontend/src/app/producto/page.tsx
@@ -1,22 +1,26 @@
 import { MenuPrincipal } from "@/components/MenuPrincipal";
 import { FProducto } from "@/components/FProducto";
+import { Button } from "@/components/ui/button"
 
 export default function productos() {
 
-  
-
   return (
-    <div className="h-screen">
-      <div>
-        <MenuPrincipal/>
+    <>
+    <div>
+      <MenuPrincipal/>
+    </div>
+    <div className="h-screen flex">
+      <div className="bg-blue-500 w-1/6 p-4">
+        <Button className="bg-blue-500 w-full h-15 hover:bg-blue-300 text-2xl">VER TODOS</Button>
+        <Button className="bg-blue-500 w-full h-15 hover:bg-blue-300 text-2xl">CONSULTAR</Button>
+        <Button className="bg-blue-500 w-full h-15 hover:bg-blue-300 text-2xl">AGREGAR</Button>
       </div>
-      <div className='flex'>
-        <div className='w-1/6 bg-blue-500'></div>
-        <div className='w-5/6 p-6'>
-         <FProducto/>
+      <div className="w-5/6">
+        <div className="p-8">
+          <FProducto/>
         </div>
       </div>
     </div>
-    
+    </>
   );
 }
